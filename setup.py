8cfe815f890fdbe5357911d72993ebd388ab7a6a
valentyn <melnychukv@cip.ifi.lmu.de>
2019-07-17 02:20:20 +0200

    Add pyseal instalation, refresh README.md

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from distutils.core import setup
 from pip.req import parse_requirements
-from distutils.command.install import install
-import setuptools.command.build_py
-import distutils.cmd
-import distutils.log
-import setuptools
-import subprocess
-import os
-from subprocess import call
-
 
 # Loading requirements
 install_reqs = parse_requirements('requirements.txt', session=False)
@@ -21,4 +12,6 @@ setup(
     author_email='v.melnychuk@campus.lmu.de, ',
     description='Implementation of Linear Regression on encrypted data with PySEAL',
     install_requires=reqs,
+    packages=['seal_regression'],
+    scripts=['install_pyseal.sh'],
 )
