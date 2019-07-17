8f9236297c492886da074c15f496bc92e2bd3fc0
valentyn <melnychukv@cip.ifi.lmu.de>
2019-07-17 12:42:36 +0200

    Fix bash script

--- install_pyseal.sh
+++ install_pyseal.sh
@@ -3,18 +3,18 @@
 sudo apt-get update
 sudo apt-get install g++ git make python3 python3-dev python3-pip sudo libdpkg-perl
 sudo rm -rf PySEAL/
-git clone https://github.com/Lab41/PySEAL.git
+sudo git clone https://github.com/Lab41/PySEAL.git
 sudo rm -rf /SEAL/
 sudo mkdir -p /SEAL/
-sudo mv PySEAL/SEAL/ /SEAL/SEAL/
-sudo mv PySEAL/SEALPython/ /SEAL/SEALPython/
+sudo cp -r PySEAL/SEAL/ /SEAL/SEAL/
+sudo cp -r PySEAL/SEALPython/ /SEAL/SEALPython/
 sudo rm -rf PySEAL/
 
 # C++ SEAL package
 cd /SEAL/SEAL/
 sudo chmod +x configure
 sudo sed -i -e 's/\r$//' configure
-./configure
+sudo ./configure
 sudo make
 
 # PySEAL package
